7a2a2f919c27cf71e5de2f19e8cc6a8768b99b21
Joakim Gottzén <joakim.gottzen@gmail.com>
2023-09-15 11:36:30 +0200

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+	java
+	id("org.springframework.boot") version "2.7.15"
+	id("io.spring.dependency-management") version "1.0.15.RELEASE"
+}
+
+group = "com.polarbookshop"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	sourceCompatibility = JavaVersion.VERSION_20
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
+
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.springframework.boot:spring-boot-starter-webflux")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
